31d8128574a05725ae198cc230b5f910f80b7c6d
Abdullah Degirmenci <abdullahdegirmenci07@gmail.com>
2024-07-27 19:40:20 +0300

    string veri türü test çalışması

--- src/js/first.js
+++ src/js/first.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+let  url ="www.kodluyoruz.org";
+let language ="Java";
+
+language = language.replace ("Java","JavaScript");
+url = url.slice(url.indexOf(".")+1);
+console.log (language);
+console.log (url);
