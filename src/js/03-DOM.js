f4b6140bceeec05c845cabd9da865a4c5f1e9383
Abdullah Degirmenci <abdullahdegirmenci07@gmail.com>
2024-07-30 13:09:07 +0300

    liste öğleri dom müdahalesi

---
+++ src/js/03-DOM.js
@@ -0,0 +1,3 @@
+let fullName =prompt("Kullanıcı Adınızı Giriniz:")
+document.getElementById("fullName").innerHTML =fullName;
+console.log (fullName);
---
+++ src/js/04-DOM.js
@@ -0,0 +1,11 @@
+let firstItem =document.querySelector("#liste>li:first-Child");
+firstItem.innerHTML+=" (ilk değer)"
+
+let ulDom =document.querySelector("#liste");
+let liDom = document.createElement("li");
+let lastItem =document.createElement("li");
+liDom.innerHTML="yeni değer";
+lastItem.innerHTML="son değer";
+ulDom.prepend(liDom);
+ulDom.append(lastItem);
+console.log (firstItem);
